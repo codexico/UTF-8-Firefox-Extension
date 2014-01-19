790179ba5818b9629231e2d00b732b2747fe0678
codexico <codexico@gmail.com>
2014-01-19 00:07:59 -0200

    version 2.0

--- defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.utfextension.boolpref", false);
-pref("extensions.utfextension.intpref", 0);
-pref("extensions.utfextension.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.utf8firefox@codexico.com.br.description", "chrome://utfextension/locale/overlay.properties");
